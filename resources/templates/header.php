17ce7a2b7c43af2c6a1f8009bf7e7a3733d43d74
Luke Cotnoir <luke.cotnoir.21@cnu.edu>
2023-02-02 11:03:04 -0500

    more css again

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -7,17 +7,14 @@
     <title>Group 1 Site</title>
     <link rel="stylesheet" href="public_html/css/header.css">
 </head>
-<body>
-<div id="header">
-    <div class="global_nav">
-        <title href="index.php">CNU Connect</title>
+<body> 
+    <div class="navbar">
+        <a href="index.php">CNU Connect</a>
 	    <a href="#">Search</a>
 	    <a href="#">Event Board</a>
         <a href="#">Communities</a>
         <a href="#">Messages</a>
         <a href="#">Login</a>
     </div>
-</div> 
-  
 </body>
-</html>
+</html> 
