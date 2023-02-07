7e42d1e67f57bcebfe88673fb1b0879bff9cbf06
Luke Cotnoir <luke.cotnoir.21@cnu.edu>
2023-02-06 21:21:45 -0500

    home page and header styling

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
-<?php
 
+<?php
     include_once(realpath("resources/config.php"));
     include_once(realpath(TEMPLATES_PATH . "/header.php"));
 ?>
-
-<div id="container">
-    <div id="content">
-        <h1>This is the home page</h1>
-        <h2>this is the test website</h2>
-        <p>I dont know what else to put here</p>
+<link href="public_html/css/home-styling.css" rel="stylesheet">
+<div class="home">
+    <div class="content">
+        <p class="greet">Welcome!<br>PCSE Student-Alumni Connection System</p>
+        <p>This system provides a platform for current PCSE students and alumni to connect.</p>
+        <p>To get started, click <a href="#">here</a> to sign in or create a new account.</p>
     </div>
 </div>
-
 <?php
     include_once(realpath(TEMPLATES_PATH . "/footer.php"));
 ?>
--- resources/templates/header.php
+++ resources/templates/header.php
@@ -5,16 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Group 1 Site</title>
-    <link rel="stylesheet" href="public_html/css/header.css">
+    <link rel="stylesheet" href="public_html/css/header-styling.css">
 </head>
 <body> 
     <div class="navbar">
-        <a href="index.php">CNU Connect</a>
-	    <a href="#">Search</a>
-	    <a href="#">Event Board</a>
-        <a href="#">Communities</a>
-        <a href="#">Messages</a>
-        <a href="#">Login</a>
+        <div class="logo"><a href="index.php">CNU Connect</a></div>
+        <div class="menu">
+	        <a href="#">Search</a>
+	        <a href="#">Event Board</a>
+            <a href="#">Communities</a>
+            <a href="#">Messages</a>
+            <a href="#">Login</a>
+        </div>
     </div>
 </body>
 </html> 
