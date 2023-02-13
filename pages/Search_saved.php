c5f03a58ab86f560ba254b7475ac3f1980ca4433
Abigail Berglie <abigail.berglie.19@cnu.edu>
2023-02-13 14:33:39 -0500

    fixed dearch

--- pages/Search_saved.php
+++ pages/Search_saved.php
@@ -7,10 +7,10 @@
     <title>search</title>
 </head>
 <body>
-<form action="searching.php" method="post">
+<form action="Search_saved.php" method="post">
 What are you searching for?:
     <select name = "dropdown">
-        <option value = "" selected> </option>
+        <option value = " " selected> </option>
         <option value = "Account" selected>Account</option>
         <option value = "Community">Community</option>
     </select>  </p>
@@ -26,23 +26,23 @@ $dbname = "project_testing";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 $searched = $_POST['searched'];
-$choice = "Community";        #Set as account now
+$choice = $_POST['dropdown'];        #Set as account now
 
-if ($choice =="Accounts"&& isset($searched) )
+if ($choice =="Account"&& isset($searched) )
 {
     echo "You searched $searched in $choice";
 
-    $sql_find =  "SELECT * FROM accounts WHERE FirstName = '$searched'";#OR
-                                               /*FirstName LIKE '%$search%' OR
-                                               LastName LIKE '%$search%' OR
-                                               StartYear LIKE '%$search%' OR
-                                               GraduationYear LIKE '%$search%' OR
-                                               Email LIKE '%$search%' OR
-                                               Acctype LIKE '%$search%' OR
-                                               Major LIKE '%$search%' OR
-                                               Minor(s) LIKE '%$search%' OR
-                                               Employer LIKE '%$search%' OR
-                                               JobTitle LIKE '%$search%'*/
+    $sql_find =  "SELECT * FROM accounts WHERE (FirstName = '$searched') OR
+                                                (LastName = '$searched') OR
+                                                (StartYear = '$searched') OR
+                                                (GraduationYear = '$searched') OR
+                                                (Email = '$searched') OR
+                                                (Acctype = '$searched') OR
+                                                (Major = '$searched') OR
+                                                (Minor(s) = '$searched') OR
+                                                (Employer = '$searched') OR
+                                                (JobTitle = '$searched')";
+                                            
                                                
     $result = $conn->query($sql_find);
     if ($result->num_rows >0){
@@ -61,27 +61,28 @@ if ($choice =="Accounts"&& isset($searched) )
 		echo "Error: ";
 	}
 }
+else
+{
+    echo "Press submit to search";
+}
+
 if ($choice == "Community")
 {
     echo "You searched $searched in $choice";
 
-    $sql_find =  "SELECT * FROM accounts WHERE FirstName = '$searched'";#OR
-                                               /*FirstName LIKE '%$search%' OR
-                                               LastName LIKE '%$search%' OR
-                                               StartYear LIKE '%$search%' OR
-                                               GraduationYear LIKE '%$search%' OR
-                                               Email LIKE '%$search%' OR
-                                               Acctype LIKE '%$search%' OR
-                                               Major LIKE '%$search%' OR
-                                               Minor(s) LIKE '%$search%' OR
-                                               Employer LIKE '%$search%' OR
-                                               JobTitle LIKE '%$search%'*/
+    $sql_find =  "SELECT * FROM accounts WHERE (ConnID = '$searched') OR
+                                                (CommName = '$searched') OR
+                                                (PCSEAffiliate = '$searched') OR
+                                                (YearCreated = '$searched') OR
+                                                (MemberCount= '$searched') OR
+                                                (PostCount = '$searched')";
+                                            
                                                
     $result = $conn->query($sql_find);
     if ($result->num_rows >0){
     echo "<br>Here are the results of your search:<br>";
     while($row = $result->fetch_assoc()){
-        echo $row['CommID'];echo "||";echo $row['Name'];echo "||";echo $row['PCSEAffiliate'];
+        echo $row['CommID'];echo "||";echo $row['CommName'];echo "||";echo $row['PCSEAffiliate'];
         echo "||";echo $row['YearCreated'];echo'||';echo $row['MemberCount'];echo'||';
         echo $row['PostCount'];
 
@@ -91,6 +92,10 @@ if ($choice == "Community")
 	else {
 		echo "Error: ";
 	}
+}
+else
+{
+    echo "Press submit to search";
 }
 
 ?>
