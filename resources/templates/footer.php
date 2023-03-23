befc63186dba0426553e5b6fae341ec4a8aaf25b
Luke Cotnoir <luke.cotnoir.21@cnu.edu>
2023-03-23 10:30:33 -0400

    added lapkes email in contact us lol

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -3,7 +3,7 @@
 <foot>
     <div id="footer">
         <div class="footer items">
-            <p>Contact us at&nbsp;<a href="helpspot@cnu.edu">helpspot@cnu.edu</a></p>
+            <p>Contact us at definitely not&nbsp;<a href="mailto: michael.lapke@cnu.edu">Dr. Michael Lapke's email...</a></p>
             <p>Report a problem with the <a href="/CPSC351-GROUP1/pages/reports/Reports_system.php">System</a> | <a href="/CPSC351-GROUP1/pages/reports/Reports_other.php">Profile, Post, Community</a></p>
         </div>
     </div>
